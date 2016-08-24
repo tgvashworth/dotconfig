4a120ad80c2167f511ece2ba5ff0dabc2a703a44
Tom Ashworth <tashworth@twitter.com>
2016-08-24 18:22:22 +0100

    service

--- aliases.zsh
+++ aliases.zsh
@@ -1,2 +1,8 @@
 alias so="cd ~/work/source"
 alias land="git ch $1 && arc land"
+function cmt() {
+    git add -p && git com "$(read -e)"
+}
+function service() {
+    curl "https://api.twitter.com$1" -H 'X-Routing-Query: 1'
+}
