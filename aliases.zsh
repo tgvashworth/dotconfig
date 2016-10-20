065efc54345094bb5941abcc6aa06897ce3ae34d
Tom Ashworth <tashworth@twitter.com>
2016-10-20 12:59:02 +0100

    zipkin util

--- aliases.zsh
+++ aliases.zsh
@@ -7,3 +7,10 @@ function cmt() {
 function service() {
     curl "https://api.twitter.com$1" -H 'X-Routing-Query: 1'
 }
+function zipkin() {
+    $@ -H 'X-B3-Flags: 1' -i \
+        | grep "x-transaction-id" \
+        | cut -d' ' -f2 \
+        | tr -d '\r' \
+        | xargs -I {} open http://go/zipkin/{}
+}
