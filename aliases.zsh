7b47eb3cb341096ee1157c5fa4a8135c9cb69b7d
Tom Ashworth <tomashworth@bulb.co.uk>
2018-11-01 15:27:48 +0000

    things

--- aliases.zsh
+++ aliases.zsh
@@ -1,40 +1,18 @@
 SCRIPT_DIR="$(dirname "$0")"
 [[ -f $SCRIPT_DIR/secrets.zsh ]] &&  source $SCRIPT_DIR/secrets.zsh
+[[ -f $SCRIPT_DIR/../work/buld/scripts/fns.sh ]] &&  source $SCRIPT_DIR/../work/buld/scripts/fns.sh
 [[ -f $HOME/.cargo/env ]] &&  source $HOME/.cargo/env
 
-export S="~/work/source"
+# How do I make local npm .bin targets available on my path?
+PATH="./node_modules/.bin:$PATH"
 
-alias so="cd ~/work/source"
-function land() {
-    git ch $1 && \
-    arc land
-}
+# How do I get completion for gloud?
+source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
+source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
+
 alias commit="git rev-parse ${1:=head}"
 function cmt() {
     git add -p && git com "$(read -e)"
-}
-function service() {
-    curl "https://api.twitter.com$1" -H 'X-Routing-Query: 1'
-}
-function zipkin() {
-    $@ -H 'X-B3-Flags: 1' -i \
-        | grep "x-transaction-id" \
-        | cut -d' ' -f2 \
-        | tr -d '\r' \
-        | xargs -I {} open http://go/zipkin/{}
-}
-function workon() {
-    NAME="$USER/$1"
-    MASTER="master"
-    BASE="${2:=$MASTER}"
-
-    echo "Working on $NAME from $BASE"
-
-    git ch $BASE
-    [[ "$BASE" == "$MASTER" ]] && git pull
-
-    (git ch $NAME || git nbr $NAME)
-    git rebase $BASE
 }
 function af() {
     PWD=$(pwd)
@@ -50,6 +28,3 @@ function greset() {
     git status --porcelain | grep -v '??' | awk '{ print $2 }' | xargs -I % sh -c 'git ch head -- "%" || trash "%";'
 }
 
-function whodis() {
-    pbpaste | tr ',' '\n' | sed -E 's/^.*<(.*)@.*>/\1/' | awk '{$1=$1;print}' | xargs -n1 -I {} -- open "http://go/who/{}"
-}
